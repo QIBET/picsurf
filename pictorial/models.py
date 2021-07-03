9d0ed62e8efab3c528432807d22bffa43cd9c1d9
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-03 13:48:27 +0300

    Creates migrations to sql

--- pictorial/models.py
+++ pictorial/models.py
@@ -3,7 +3,7 @@ from django.db import models
 class Image(models.Model):
     image_name =models.CharField(max_length =30)
     description =models.CharField(max_length =50)
-    image = models.ImageField(upload_to = 'pic/', default='image',null = True)
+    image = models.ImageField(upload_to = 'pictures/', default='image',null = True)
     location = models.ForeignKey('Location', on_delete = models.CASCADE)
     category = models.ForeignKey('Category', on_delete = models.CASCADE)
 
