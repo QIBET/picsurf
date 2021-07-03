fc30f1755615c4ca9b5e354f7dcbbcdc3b5e2025
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-03 13:13:24 +0300

    Creates database

--- picsurf/settings.py
+++ picsurf/settings.py
@@ -77,8 +77,10 @@ WSGI_APPLICATION = 'picsurf.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'picsurf',
+        'USER': 'moringa',
+    'PASSWORD':'Sotik2020*',
     }
 }
 
@@ -107,7 +109,7 @@ AUTH_PASSWORD_VALIDATORS = [
 
 LANGUAGE_CODE = 'en-us'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Africa/Nairobi'
 
 USE_I18N = True
 
