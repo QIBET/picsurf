3b5189f5ba7da78a97c94aa6e0322c9afefe2112
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-03 13:38:41 +0300

    Creating models location, category and Image

--- pictorial/models.py
+++ pictorial/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Image(models.Model):
+    image_name =models.CharField(max_length =30)
+    description =models.CharField(max_length =50)
+    image = models.ImageField(upload_to = 'pic/', default='image',null = True)
+    location = models.ForeignKey('Location', on_delete = models.CASCADE)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE)
+
+class Location(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
+class Category(models.Model):
+    name = models.CharField(max_length =30)
+
+    def __str__(self):
+        return self.name
