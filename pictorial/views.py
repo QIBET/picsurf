70b130337f727cd8364f9c66660136414d708932
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-03 22:25:43 +0300

    Creates an upload path for images

--- pictorial/views.py
+++ pictorial/views.py
@@ -5,5 +5,5 @@ from pictorial.models import Image
 # Create your views here.
 def pictorial(request):
     pics = Image.all_photos()
-    return render(request, 'pictorial.html',{"all_pics":pics})
+    return render(request, 'pictorial.html',{"all_photos":pics})
 
