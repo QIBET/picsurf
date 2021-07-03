ae742d1272861ed115c00b8670931136e3628e79
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-03 14:05:37 +0300

    tests image module instance

--- pictorial/tests.py
+++ pictorial/tests.py
@@ -1,3 +1,7 @@
 from django.test import TestCase
+from .models import Image,Category,Location
 
-# Create your tests here.
+class ImageTestClass(TestCase):
+    def setUp(self):
+        self.new_image= Image(image_name = 'burger', description ='Food and snacks', location = "Nairobi",category = "foods")
+
