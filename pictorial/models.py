cac43543e5450fa3ac5cea73b221f767d75fdc4a
bernard kibet <bernard.kibet@student.moringaschool.com>
2021-07-03 19:12:47 +0300

    creates a django admin

--- pictorial/models.py
+++ pictorial/models.py
@@ -4,8 +4,16 @@ class Image(models.Model):
     image_name =models.CharField(max_length =30)
     description =models.CharField(max_length =50)
     image = models.ImageField(upload_to = 'pictures/', default='image',null = True)
-    location = models.ForeignKey('Location', on_delete = models.CASCADE)
-    category = models.ForeignKey('Category', on_delete = models.CASCADE)
+    location = models.ForeignKey('Location', on_delete = models.CASCADE,null='True', blank=True)
+    category = models.ForeignKey('Category', on_delete = models.CASCADE,null='True', blank=True)
+
+    def __str__(self):
+        return self.name
+    class Meta:
+        ordering = ['image_name']
+
+    def save_image(self):
+        self.save() 
 
 class Location(models.Model):
     name = models.CharField(max_length =30)
--- pictorial/tests.py
+++ pictorial/tests.py
@@ -3,5 +3,14 @@ from .models import Image,Category,Location
 
 class ImageTestClass(TestCase):
     def setUp(self):
-        self.new_image= Image(image_name = 'burger', description ='Food and snacks', location = "Nairobi",category = "foods")
+        self.new_image= Image(image = 'burger', description ='Food and snacks')
 
+    # Testing  instance
+    def test_instance(self):
+        self.assertTrue(isinstance(self.new_image,Image))
+     # Testing Save Method
+
+    def test_save_image(self):
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images) > 0)
